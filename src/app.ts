69315c5ce07dc8a85bd5dfff66b7e53af812cd38
Maho Morichika <nat@magicnat.com>
2020-09-14 01:03:36 -0400

    color update

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const term = new Terminal({
         background: '#1D1F21',
         cursor: '#C5C8C6',
         cursorAccent: '#C5C8C6',
-        black: '#373B41',
+        black: '#555555',
         red: '#CC6666',
         green: '#B5BD68',
         yellow: '#F0C674',
@@ -42,7 +42,7 @@ const bgpLinkAddon = new BgpLinkAddon();
 term.loadAddon(bgpLinkAddon);
 
 term.open(document.getElementById('terminal'));
-term.write(`\x1b[1;30mnato webrs client ${pkg.version} ready.\x1b[0m\r\n`);
+term.write(`\x1b[0;30mnato webrs client ${pkg.version} ready.\x1b[0m\r\n`);
 
 if (window.visualViewport) { // fucking iOS Safari
     document.documentElement.addEventListener('touchmove', e => e.preventDefault(), { passive: false });
@@ -52,7 +52,7 @@ if (window.visualViewport) { // fucking iOS Safari
     };
 } else window.onresize = () => handleSizeChange();
 
-term.write(`Trying ${url}...\r\n`);
+term.write(`\x1b[0;30mTrying ${url}...\x1b[0m\r\n`);
 
 let ws = new WebSocket(`${url}/`);
 const attachAddon = new AttachAddon(ws);
@@ -60,7 +60,7 @@ term.loadAddon(attachAddon);
 
 ws.onopen = () => {
     handleSizeChange();
-    term.write(`Connected to ${url}.\r\n`);
+    term.write(`\x1b[0;30mConnected to ${url}.\x1b[0m\r\n`);
     term.focus();
 
     let hash = window.location.hash;
@@ -69,5 +69,5 @@ ws.onopen = () => {
         ws.send(`${window.decodeURIComponent(hash.replace('#', ''))}\r`);
     }
 }
-ws.onerror = () => term.write('Connection reset.\r\n');
-ws.onclose = () => term.write('Connection closed by foreign host.\r\n');
+ws.onerror = () => term.write('\x1b[0;30mConnection reset.\x1b[0m\r\n');
+ws.onclose = () => term.write('\x1b[0;30mConnection closed by foreign host.\x1b[0m\r\n');
